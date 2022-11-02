dae15b49f04cbeed49dd4336ea8865749ecb071a
Jowhara <89601541+Jowhara@users.noreply.github.com>
2022-11-02 11:02:11 +0100

    update
    adding data to product.

--- src/product.js
+++ src/product.js
@@ -9,6 +9,19 @@ const Product = props => {
         className={"image-product"}
       />
       <h3>{name}</h3>
+      <p>{description}</p>
+      <span>${price}</span>
+      <div>
+        <button
+          className="snipcart-add-item"
+          data-item-id={id}
+          data-item-image={imageUrl}
+          data-item-name={name}
+          data-item-url="/"
+          data-item-price={price}>
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 };
